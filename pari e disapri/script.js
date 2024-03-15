0d118e69330fa17d6d780842324189f75cb6e094
Manuel Maisano <manumaisano@gmail.com>
2024-03-15 09:55:46 +0100

    add random function

--- pari e disapri/script.js
+++ pari e disapri/script.js
@@ -1,5 +1,9 @@
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// funzione per generare un numero random preso da w3school
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
@@ -20,3 +24,6 @@ console.log('Numero user', + UserNumber);
 
 
 
+// numero random computer
+const PcNumber = getRndInteger(1, 5);
+console.log('Numero computer', + PcNumber);
