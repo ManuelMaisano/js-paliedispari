48f181cefe79e44e3e14dbacd92189cbecbc736f
Manuel Maisano <manumaisano@gmail.com>
2024-03-15 10:09:32 +0100

    even or odd

--- pari e disapri/script.js
+++ pari e disapri/script.js
@@ -1,9 +1,9 @@
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // funzione per generare un numero random preso da w3school
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+//function getRndInteger(min, max) {
+//    return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
@@ -19,11 +19,23 @@ while (userChoice !== 'pari' && userChoice !== 'dispari') {
 
 //l'utente sceglie un numero tra 1 e 5
 
-const UserNumber = parseInt(prompt('Dimmi un numero da 1 a 5'));
-console.log('Numero user', + UserNumber);
+const userNumber = parseInt(prompt('Dimmi un numero da 1 a 5'));
+console.log('Numero user', + userNumber);
 
 
 
-// numero random computer
-const PcNumber = getRndInteger(1, 5);
-console.log('Numero computer', + PcNumber);
+// numero random computerpari
+const pcNumber = getRndInteger(1, 5);
+console.log('Numero computer', + pcNumber);
+
+
+// eseguiamo somma dei due numeri
+const resultNumbers = sumOfNumber (userNumber, pcNumber);
+console.log(resultNumbers);
+
+
+const resultEvenOrOdd = evenOrOdd(resultNumbers)
+console.log(resultEvenOrOdd);
+
+const winOrLoseMessage = userWinOrLose (userChoice, resultEvenOrOdd)
+alert(winOrLoseMessage);
