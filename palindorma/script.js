9577507721711919ebd8247a4fe70ae5275fb5dc
Manuel Maisano <manumaisano@gmail.com>
2024-03-15 10:20:33 +0100

    add function palindrome

--- palindorma/script.js
+++ palindorma/script.js
@@ -8,3 +8,21 @@ const userWord = prompt('Scrivi una parola')
 const wordPalindrome = isTheWordAPalindrom (userWord)
 alert (wordPalindrome)
 console.log(wordPalindrome);
+ 
+
+// FUNCTIONS
+function isTheWordAPalindrom (word) {
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i-- ) {
+        let letterWord = word[i];
+        console.log(letterWord);
+        reverseWord += letterWord;
+    }
+    let palindromOrNot;
+    if (reverseWord === word) {
+        palindromOrNot = "È palindromo"
+    } else {
+        palindromOrNot = "Non è palindromo"
+    }
+    return palindromOrNot
+}
