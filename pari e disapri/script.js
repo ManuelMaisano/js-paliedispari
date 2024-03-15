83ca733810293a6a3d0a2d8d7dc8e9409c486e52
Manuel Maisano <manumaisano@gmail.com>
2024-03-15 09:51:35 +0100

    add index and js

---
+++ pari e disapri/script.js
@@ -0,0 +1,17 @@
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
+
+//scegliamo tra pari e dispari
+
+let userChoice = prompt('Scegli tra pari e dispari');
+console.log(userChoice);
+
+while (userChoice !== 'pari' && userChoice !== 'dispari') {
+    userChoice = prompt('Scegli tra pari e dispari');
+}
+
+
+
